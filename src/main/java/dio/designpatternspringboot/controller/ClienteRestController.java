d8801b3a4bdb233c2cb1d3e35e04d6d15a20035b
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-31 08:48:51 -0300

    add ClienteRestController

---
+++ src/main/java/dio/designpatternspringboot/controller/ClienteRestController.java
@@ -0,0 +1,42 @@
+package dio.designpatternspringboot.controller;
+
+import dio.designpatternspringboot.model.Cliente;
+import dio.designpatternspringboot.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("clientes")
+public class ClienteRestController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Cliente>> buscarTodos(){
+        return ResponseEntity.ok(clienteService.buscarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(clienteService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente){
+        clienteService.inserir(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
+        clienteService.atualizar(id, cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        clienteService.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+}
