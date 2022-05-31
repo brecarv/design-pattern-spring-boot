653ababf804e49caf6808b1e8d139848e8e3c806
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-31 08:37:51 -0300

    add cliente/endereco model and repository

---
+++ src/main/java/dio/designpatternspringboot/model/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package dio.designpatternspringboot.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+}
