17e904c46ab9703c552badcedeb5c00650af1a21
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-31 09:28:25 -0300

    ClienteServiceImpl: inserir implemented

--- src/main/java/dio/designpatternspringboot/service/impl/ClienteServiceImpl.java
+++ src/main/java/dio/designpatternspringboot/service/impl/ClienteServiceImpl.java
@@ -2,7 +2,10 @@ package dio.designpatternspringboot.service.impl;
 
 import dio.designpatternspringboot.model.Cliente;
 import dio.designpatternspringboot.model.ClienteRepository;
+import dio.designpatternspringboot.model.Endereco;
+import dio.designpatternspringboot.model.EnderecoRepository;
 import dio.designpatternspringboot.service.ClienteService;
+import dio.designpatternspringboot.service.ViaCepService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,11 @@ public class ClienteServiceImpl implements ClienteService {
     // TODO Singleton: Injetar os componentes do Spring com @Autowired.
     @Autowired
     private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+
     // TODO Strategy: Implementar os métodos definidos na interface.
     // TODO Facade: Abstrair integrações com subsistemas, provendo uma interfae simples.
 
@@ -30,7 +38,17 @@ public class ClienteServiceImpl implements ClienteService {
 
     @Override
     public void inserir(Cliente cliente) {
-
+        // Verifica se o endereço do cliente já existe.
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            // Se não existir, usar o ViaCep para persistir o retorno.
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        // Inserir o Cliente com endereço (novo ou existente).
+        clienteRepository.save(cliente);
     }
 
     @Override
